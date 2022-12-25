2bfa0bb9d35a3b5f799e1256600738670d49afa6
ConnerFisk <u1354765@umail.utah.edu>
2022-12-24 20:43:54 -0700

    Changed formatting & added music option/hyperlinks

--- PlanetSite/main.js
+++ PlanetSite/main.js
@@ -8,12 +8,9 @@ const scene = new THREE.Scene()
 
 //Create our planet
 const geometry = new THREE.IcosahedronGeometry(3, 1)
-// const material = new THREE.MeshStandardMaterial({
-//   color: "#ff634e",
-// })
 const material = new THREE.MeshBasicMaterial({
   wireframe: true,
-  color: '#000000',
+  color: '#ffffff',
 })
 const mesh = new THREE.Mesh(geometry, material);
 scene.add(mesh)
@@ -111,6 +108,49 @@ document.querySelectorAll('a[href^="#"]').forEach(anchor => {
     })
   })
 })
+
+gsap.registerPlugin(ScrollTrigger);
+// REVEAL //
+gsap.utils.toArray(".revealUp").forEach(function (elem) {
+  ScrollTrigger.create({
+    trigger: elem,
+    start: "top 80%",
+    end: "bottom 20%",
+    markers: true,
+    onEnter: function () {
+      gsap.fromTo(
+        elem,
+        { y: 100, autoAlpha: 0 },
+        {
+          duration: 1.25,
+          y: 0,
+          autoAlpha: 1,
+          ease: "back",
+          overwrite: "auto"
+        }
+      );
+    },
+    onLeave: function () {
+      gsap.fromTo(elem, { autoAlpha: 1 }, { autoAlpha: 0, overwrite: "auto" });
+    },
+    onEnterBack: function () {
+      gsap.fromTo(
+        elem,
+        { y: -100, autoAlpha: 0 },
+        {
+          duration: 1.25,
+          y: 0,
+          autoAlpha: 1,
+          ease: "back",
+          overwrite: "auto"
+        }
+      );
+    },
+    onLeaveBack: function () {
+      gsap.fromTo(elem, { autoAlpha: 1 }, { autoAlpha: 0, overwrite: "auto" });
+    }
+  });
+});
 
 
 
